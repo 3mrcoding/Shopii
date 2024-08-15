3e609ced994ec6de07f1d70a313647a49e797eb1
mkkmsotafa12 <kamalmostafa2@github.com>
2024-08-15 22:42:30 +0300

    modifiy app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
+const AppError = require("./util/AppError");
+const errController = require("./controller/errController");
 
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // middlewares
+app.all("*", (req, res, next) => {
+  next(new AppError("Requested Route not found!", 404));
+});
+
+app.use(errController);
 
 module.exports = app;
